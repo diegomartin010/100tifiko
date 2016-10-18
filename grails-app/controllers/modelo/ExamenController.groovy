6c89c4757f9bc21cef0e01a960973ef8d9bab9d4
Diego Colussi <diegomartin010@gmail.com>
2016-10-17 21:00:51 -0300

    Arreglando cosas atadas con alambre.

--- grails-app/controllers/modelo/ExamenController.groovy
+++ grails-app/controllers/modelo/ExamenController.groovy
@@ -10,8 +10,8 @@ class ExamenController {
     def getExamenes(){
         def user = SessionManager.getCurrentUser()
         def result = []
-        user.estadoAcademico.examenes.findall().each{ex->
-            result.push(examen:ex)
+        user.estadoAcademico.examenes.findAll().each{ex->
+            result.push(ex)
         }
         render(contentType: 'text/json') {
             result
@@ -32,29 +32,62 @@ class ExamenController {
     }
 
     def guardar(){
-    	def fec = request.getParameter("fecha")
-    	def mat = request.getParameter("materia")
-    	def nota = request.getParameter("nota")
-    	//capturo el usuario
-    	def user = SessionManager.getCurrentUser()
+    	
 
-    	//básicamente añado a la materia, la fecha y nota del examen.
-    	//además, si la nota es mayor o igual a 4, cambio el estado a aprobado.
-    		def m = user.estadoAcademico.estadoMaterias.find { it.materia == mat }	
-    		//if (nota >= 4){
-    		//	m.estado="A"
-            //}
-            //creo el examen y lo guardo.
-            user.estadoAcademico.examenes.push(
-                new Examen (
-                    fecha  : fec,
-                    materia : mat,
-                    nota : nota
-                ).save()
-            )
-    		//guardo
-    		//m.save()
-    		//valor de retorno
+        println("Controlador guardar Examenes")
+        
+        
+
+        // Usuario en sesion.
+    	def user            = SessionManager.getCurrentUser()
+        // Estado academico del tipo.
+        def ea              = user.estadoAcademico
+
+        
+
+        // Parametros de formulario por POST.
+        // Convertimos fecha ingresado como string, en una fecha piola.
+        def fecha           = new Date().parse("dd/MM/yy" , params.fecha)
+        // Este queda como esta ...
+        def nombreMateria   = params.materia
+        // El formulario lo pasa en String, el dato tiene que ser en integer.
+        def nota            = params.nota.toInteger()          
+        println("Parametros Fecha: ${fecha} Materia:${nombreMateria} Nota:${nota}")
+        
+
+
+        // Busco en la carrera del tipo, si hay una materia con el nombre pasado en el param.
+        def laMateria = 
+            ea
+            .carrera
+            .materias
+            .find{ it.nombre == nombreMateria }
+        
+
+
+        println("voy a crear un examen con materia ${laMateria.nombre} ${laMateria.codigo}")
+        def nex = 
+            new Examen (
+                // Paso parametro de fecha.
+                fecha:  fecha,
+                // Paso la materia recien traida.
+                materia : laMateria,
+                // Paso la nota en integer. El atributo del examen es Calificacion.
+                calificacion : nota,
+                // No cargo ninguna observacion.
+                observaciones: null
+            ).save( flush:true )
+        ea.examenes.push(nex)
+        
+        println("guardado")
+        
+        user.save( flush:true )
+        
+        // Martin es un Tape!!!.
+        render(contentType: 'text/json') {[
+            examen: nex,
+            result: true
+        ]}
     		
 
     } 
--- grails-app/views/examen/index.gsp
+++ grails-app/views/examen/index.gsp
@@ -12,7 +12,7 @@
 	<g:javascript>
 	//genero la lista de exámenes rendidos para poner en la tabla
 	$(document).ready(function(){
-		$post("examen/getExamenes", function (data){
+		$.post("examen/getExamenes", function (data){
 			$each( data, function(index,ex){
 				notaExamen = ex.calificacion;
 				fechaExamen = ex.fecha;
@@ -65,12 +65,18 @@
 				    cell2.innerHTML = examen.materia;
 				    cell3.innerHTML = examen.nota; */
 
+				    console.log(examen)
 				    //agrego funcionalidad para almacenar el examen.
 				    $.post( "/examen/guardar",{
 					    fecha: $("#datepicker").val(),
 						materia: $("#idmateria").val(),
 						nota: $("#idnota").val()
-				    });
+				    }).done(function( resp ){
+				    	if(resp.result){
+	   					 	alert("El examen se cargo! Ver JSON Resultante en consola !!!");
+	   					 	console.log(resp.examen);
+				    	}
+  					});
 
 
 		}
