d068cdc441d51faa7d9be496fb57a64a7703f91a
martindegesus <martindg_72@hotmail.com>
2016-10-21 22:33:06 -0300

    funcionalidades del módulo de estadísticas, faltan unas pocas

--- grails-app/views/estadisticas/index.gsp
+++ grails-app/views/estadisticas/index.gsp
@@ -22,43 +22,43 @@
 				<div class="container">
 					<div class="panel panel-default">
 						<div class="panel-heading">Promedio con aplazos</div>
-						<div class="panel-body"> 2.75 (hardcode)</div>
-					</div>
-					<div class="panel panel-default">
-						<div class="panel-heading">Promedio con aplazos</div>
-						<div class="panel-body"> 2.75 (hardcode)</div>
+						<div id="1" class="panel-body"> </div>
 					</div>
 					<div class="panel panel-default">
 						<div class="panel-heading">Promedio sin aplazos</div>
-						<div class="panel-body"> 4 (hardcode)</div>
+						<div id="2" class="panel-body"> 4 (hardcode)</div>
 					</div>
 					<div class="panel panel-default">
 						<div class="panel-heading">Cantidad de materias aprobadas</div>
-						<div class="panel-body"> 1 (hardcode)</div>
+						<div id="3" class="panel-body"> 1 (hardcode)</div>
 					</div>
 					<div class="panel panel-default">
 						<div class="panel-heading">Cantidad de finales desaprobados</div>
-						<div class="panel-body"> 3 (hardcode)</div>
+						<div id="4" class="panel-body"> 3 (hardcode)</div>
 					</div>
 					<div class="panel panel-default">
 						<div class="panel-heading">Cantidad de materias regularizadas</div>
-						<div class="panel-body"> 3 (hardcode)</div>
+						<div id="5" class="panel-body"> 3 (hardcode)</div>
 					</div>
 					<div class="panel panel-default">
 						<div class="panel-heading">Cantidad de materias que cursa actualmente</div>
-						<div class="panel-body"> 5 (hardcode)</div>
+						<div id="6" class="panel-body"> 5 (hardcode)</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading">Cantidad de materias pendientes</div>
+						<div id="7" class="panel-body"> 35 (hardcode)</div>
 					</div>
 					<div class="panel panel-default">
 						<div class="panel-heading">Tiempo que le está tomando la carrera</div>
-						<div class="panel-body"> 8 años (hardcode)</div>
+						<div id="8" class="panel-body"> 8 años (hardcode)</div>
 					</div>
 					<div class="panel panel-default">
 						<div class="panel-heading">Porcentaje de realización de la carrera</div>
-						<div class="panel-body"> 4.5%(hardcode)</div>
+						<div id="9" class="panel-body"> 4.5%(hardcode)</div>
 					</div>
 					<div class="panel panel-default">
 						<div class="panel-heading">Tiempo hasta próximo examen</div>
-						<div class="panel-body"> 3 dias (hardcode)</div>
+						<div id="10" class="panel-body"> 3 dias (hardcode)</div>
 					</div>
 				</div>
 				</form>
--- web-app/js/estadisticas/estadisticas.js
+++ web-app/js/estadisticas/estadisticas.js
@@ -1,5 +1,76 @@
 //js Usado en "/views/estadisticas/index.gsp"
 
-$(document).ready(function)(){
-	console.log("hola mundo");
-}
+$(document).ready(function(){
+	calcularExamenes();
+	calcularMaterias();
+});
+
+	function calcularExamenes(){
+		var contador = 0;
+		var contadorAprob = 0;
+		var acumulador = 0;
+		var acumuladorAprob = 0;
+		var contadorDesaprob = 0;
+		$.post("examen/getExamenes",function(data){
+			$.each(data, function(index, examen){
+				if(examen.nota>3){
+					acumuladorAprob+=examen.nota;
+					contadorAprob++;
+				}else{
+					contadorDesaprob++;
+				}
+				acumulador+=examen.nota;
+				contador++;
+			});
+		var promedioConAplazos = acumulador/contador;
+		console.log ("promedio con aplazos: "+promedioConAplazos);
+		document.getElementById("1").innerHTML = promedioConAplazos;
+		var promedioSinAplazos = acumuladorAprob/contadorAprob;
+		console.log("promedio sin aplazos: "+promedioSinAplazos);
+		document.getElementById("2").innerHTML = promedioSinAplazos;
+		//ESTO ÑO: console.log("cantidad de materias aprobadas: "+contadorAprob);
+		console.log("cantidad de finales desaprobados: "+contadorDesaprob);
+		document.getElementById("4").innerHTML = contadorDesaprob;
+		});
+	};
+	function calcularMaterias(){
+		var materiasAprobadas;
+		//calculo materias aprobadas
+		$.post("estadoAcademico/getMateriasByEstado",{
+        	estado: "A"
+        }).done(function(d) {
+         	 materiasAprobadas= d.length;
+        	console.log("cantidad de materias aprobadas: "+materiasAprobadas);
+        	document.getElementById("3").innerHTML = materiasAprobadas;
+		});
+
+        //calculo materias regularizadas
+		$.post("estadoAcademico/getMateriasByEstado",{
+        	estado: "R"
+        }).done(function(d) {
+         		var materiasRegularizadas= d.length;
+		        console.log("cantidad de materias regularizadas: "+materiasRegularizadas);
+		        document.getElementById("5").innerHTML = materiasRegularizadas;
+		});
+        //calculo materias cursando actualmente
+		$.post("estadoAcademico/getMateriasByEstado",{
+			estado: "C"
+		}).done(function(d){
+			var materiasEnCurso = d.length;
+			console.log("cantidad de materias cursando: "+materiasEnCurso);
+			document.getElementById("6").innerHTML = materiasEnCurso;
+		});
+		//Esto se viene romerizado:
+		//Agregar materias pendientes a las estadísticas del alumno
+		$.post("estadoAcademico/getMateriasByEstado",{
+			estado: "P"
+		}).done(function(d){
+			var materiasPendientes = d.length;
+			console.log("cantidad de materias pendientes: "+materiasPendientes);
+			document.getElementById("7").innerHTML = materiasPendientes;
+		});
+		//var totalMaterias = 0;
+		//totalMaterias = materiasAprobadas+materiasRegularizadas+materiasEnCurso+materiasPendientes;
+		//var porcentajeDeRealizacion = materiasAprobadas/totalMaterias;
+		//console.log("porcentaje de realización de la carrera: "+porcentajeDeRealizacion);
+	};
