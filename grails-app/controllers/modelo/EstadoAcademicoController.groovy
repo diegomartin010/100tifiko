9aae1a4061da26b52acd7b22d42fb45514b0b381
DMColussi <diegomartin010@gmail.com>
2016-11-05 08:30:30 -0300

    Commit para molestar al mudo

--- grails-app/controllers/modelo/EstadoAcademicoController.groovy
+++ grails-app/controllers/modelo/EstadoAcademicoController.groovy
@@ -15,7 +15,7 @@ class EstadoAcademicoController {
     }
 
     def crear() { 
-        
+        // Molestar al mudo 5/11 8:29am
         try {
         
             // Se captura el legajo y el id del formulario y se parse a Integer.            
